6efd2b67fee4a7ad3adb6e655c99751127ba3bf8
Mohammed Kabir Hussaini <65419835+lekandev@users.noreply.github.com>
2021-05-12 17:56:13 +0100

    .

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,11 +1,19 @@
 <template>
   <header class="text-center bg-blue-800 text-white p-4 mb-10">
-    <div class="text-3xl md:text-5xl font-bold mb-3">Covid-19 Tracker</div>
+    <div class="text-3xl md:text-5xl font-bold mb-3">
+      <i class="fa fa-viruses"></i> Covid-19 Tracker
+    </div>
+    <p>
+      API by
+      <a href="https://covid10api.com" class="text-blue-300" target="_blank"
+        >covid19api.com</a
+      >
+    </p>
   </header>
 </template>
 
 <script>
 export default {
-  name: 'Header'
-}
+  name: "Header",
+};
 </script>
