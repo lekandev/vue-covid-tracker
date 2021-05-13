33df83df0ceb24532a4b5c61eb29792cd1b6dcdd
Mohammed Kabir Hussaini <65419835+lekandev@users.noreply.github.com>
2021-05-13 03:10:34 +0100

    Update DataBoxes.vue

--- src/components/DataBoxes.vue
+++ src/components/DataBoxes.vue
@@ -34,7 +34,7 @@
 export default {
     name: 'DataBoxes',
     props: ['stats'],
-    methods: {
+    setup(): {
         numberWithCommas(x) {
             return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
         }
